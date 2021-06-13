f6d8d753f3ce0e4958a19a189f84ae367f689782
Nick Robson <robson.nicholas1@gmail.com>
2021-06-14 01:45:03 +1000

    Clean up data when refresh token has been revoked

--- src/main/java/xyz/nickr/telegram/nowlistening/scheduler/AuthorisationRefresher.java
+++ src/main/java/xyz/nickr/telegram/nowlistening/scheduler/AuthorisationRefresher.java
@@ -2,10 +2,13 @@ package xyz.nickr.telegram.nowlistening.scheduler;
 
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.exceptions.detailed.BadRequestException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Set;
+
 import lombok.AllArgsConstructor;
 import xyz.nickr.telegram.nowlistening.db.DatabaseController;
 import xyz.nickr.telegram.nowlistening.db.models.SpotifyUser;
@@ -34,6 +37,24 @@ public class AuthorisationRefresher implements Runnable {
 
                     spotifyController.updateSpotifyUser(user.getTelegramUserId(), credentials);
                     System.out.format("[NowListening] Refreshed tokens for %d.\n", user.getTelegramUserId());
+                } catch (BadRequestException ex) {
+                    if ("Refresh token revoked".equals(ex.getMessage())) {
+                        try {
+                            databaseController.deleteSpotifyUser(user.getTelegramUserId());
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            databaseController.deletePlayingData(user.getTelegramUserId());
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            databaseController.deleteAllMessages(user.getTelegramUserId());
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 } catch (SQLException | SpotifyWebApiException | IOException ex) {
                     ex.printStackTrace();
                 }
